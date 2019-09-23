6d93cbcc42296b88e121d10d2632d58b648275c8
Jaime Milne <milnejaim@AD.ECS.VUW.AC.NZ>
2019-09-23 12:40:44 +1200

    Worked with cameron to get menus workings and scene changes on both button and object touch

---
+++ VRTigo/Assets/SceneChangeButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeButton : MonoBehaviour
+{
+    public Button button;
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = gameObject.GetComponent<Button>();
+        button.onClick.AddListener(delegate { SceneChange(); });
+    }
+
+    // Update is called once per frame
+    void SceneChange()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
---
+++ VRTigo/Assets/SceneChangeOnTouch.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChangeOnTouch : MonoBehaviour
+{
+    public Collider col;
+    public string sceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = gameObject.GetComponent<Collider>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
