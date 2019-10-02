75e5099c46324bc95a99bf38bf68a23cc75f5eaf
scotthbran <scotthbran@ecs.vuw.ac.nz>
2019-10-03 12:31:12 +1300

    Add 2nd protal & Add screenshots & video of portal rendering

---
+++ VRTigo/Assets/Portal/PortalTeleporter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalTeleporter : MonoBehaviour
+{
+
+    public Transform player;
+    public Transform receiver; 
+    
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+        }
+    }
+}
--- VRTigo/Assets/Portal/PortalTextureSetup.cs
+++ VRTigo/Assets/Portal/PortalTextureSetup.cs
@@ -8,6 +8,9 @@ public class PortalTextureSetup : MonoBehaviour
     public Camera cameraB;
     public Material cameraMatB;
     
+    public Camera cameraA;
+    public Material cameraMatA;
+    
     void Start() {
         if (cameraB.targetTexture != null)
         {
@@ -15,6 +18,14 @@ public class PortalTextureSetup : MonoBehaviour
         }
         cameraB.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
         cameraMatB.mainTexture = cameraB.targetTexture;
+        
+        if (cameraA.targetTexture != null)
+        {
+            cameraA.targetTexture.Release();
+        }
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
+        
     }
 
 }
