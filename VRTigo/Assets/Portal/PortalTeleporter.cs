3cf3fe906e6c42242b6c3a2bbbc8baa235ca3442
Nicholas Snellgrove <snellgnich@ecs.vuw.ac.nz>
2019-10-04 16:03:22 +1300

    Add teleporting

--- VRTigo/Assets/Portal/PortalTeleporter.cs
+++ VRTigo/Assets/Portal/PortalTeleporter.cs
@@ -15,7 +15,22 @@ public class PortalTeleporter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerIsOverlapping) {
+            Debug.Log("hi");
+            Vector3 portalToPlayer = player.position - transform.position;
+            float dotProduct = Vector3.Dot(transform.up, portalToPlayer);
+
+            if (dotProduct < 0f) {
+                float rotationDiff = -Quaternion.Angle(transform.rotation, receiver.rotation);
+                rotationDiff += 180;
+                player.Rotate(Vector3.up, rotationDiff);
+
+                Vector3 positionOffest = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+                player.position = receiver.position + positionOffest;
+
+                playerIsOverlapping = false;
+            }
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,6 +43,8 @@ public class PortalTeleporter : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        
+        if (other.CompareTag("Player")) {
+            playerIsOverlapping = false;
+        }
     }
 }
