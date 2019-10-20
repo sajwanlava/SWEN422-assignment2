20a78d64adb30f576a0d832e8623a0824c412692
scotthbran <scotthbran@ecs.vuw.ac.nz>
2019-10-20 14:52:47 +1300

    fixing commit? @milnejaim #13

---
+++ VRTigo/Assets/CollectibleBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class CollectibleBehaviour : MonoBehaviour
+{
+    public float rotateX = 0;
+    public float rotateY = 0;
+    public float rotateZ = 0;
+
+    public float value = 300;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateX, rotateY, rotateZ);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (other.gameObject.tag == "Player")
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+}
